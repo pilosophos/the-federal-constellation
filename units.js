5621321d15d98206149f1d589aa3cff8af693160
pilosophos <48043580+pilosophos@users.noreply.github.com>
2022-09-22 00:32:10 -0700

    add more units

--- units.js
+++ units.js
@@ -1,7 +1,7 @@
-// TIME
+// TIME: 1 chron = 12^12 periods of caesium transition radiation
 
-let CAESIUM_FREQ = 9192631770;
-let PERIODS = Math.pow(12, 12);
+const CAESIUM_FREQ = 9192631770;
+const PERIODS = Math.pow(12, 12);
 
 function chronsToMetricTime(chrons) {
   let seconds = chrons * (PERIODS / CAESIUM_FREQ);
@@ -10,7 +10,12 @@ function chronsToMetricTime(chrons) {
   let days = hours / 24;
   let months = days / 30;
   let years = days / 365;
+
+  let milliseconds = seconds * 1e3;
+  let nanoseconds = seconds * 1e9;
   return {
+    nanoseconds: nanoseconds,
+    milliseconds: milliseconds,
     seconds: seconds,
     minutes: minutes,
     hours: hours,
@@ -20,18 +25,96 @@ function chronsToMetricTime(chrons) {
   };
 }
 
-function fillMetricTimeWith(chrons) {
-  for (let [unit, value] of Object.entries(chronsToMetricTime(chrons))) {
+// LENGTH: 1 tetra = distance covered by light in 1/(12^12) chrons
+
+const LIGHT_SPEED = 299792458;
+const CHRON = chronsToMetricTime(1).seconds;
+const TETRA_DIVISOR = Math.pow(12, 12);
+
+const SECONDS_PER_YEAR = 3.154e7; // for light year conversion
+
+function tetrasToMetricLength(tetras) {
+  let metres = tetras * LIGHT_SPEED * (CHRON / TETRA_DIVISOR)
+  return {
+    nanometres: metres * 1e9,
+    millimetres: metres * 1e3,
+    centimetres: metres * 1e2,
+    metres: metres,
+    kilometres: metres / 1e3,
+    lightyears: metres / (LIGHT_SPEED * SECONDS_PER_YEAR)
+  };
+}
+
+// DOM MANIPULATION
+const SI_PREFIXES = {
+  // "yotta":  24,
+  // "zetta":  21,
+  "exa":    18,
+  "peta":   15,
+  "tera":   12,
+  "giga":   9,
+  "mega":   6,
+  "kilo":   3,
+  "hecto":  2,
+  "deca":   1,
+  // "yocto":  -24,
+  // "zepto":  -21,
+  // "atto":   -18,
+  // "femto":  -15,
+  // "pico":   -12,
+  "nano":   -9,
+  "micro":  -6,
+  "milli":  -3,
+  "centi":  -2,
+  "deci":   -1,
+}
+
+const CONSTELLATION_UNITS = {
+  chron: {
+    inputValueID: "chrons",
+    unitMagnitudeID: "chronMagnitude",
+    convertFunc: chronsToMetricTime
+  },
+  tetra: {
+    inputValueID: "tetras",
+    unitMagnitudeID: "tetraMagnitude",
+    convertFunc: tetrasToMetricLength
+  }
+}
+
+for (const prefix in SI_PREFIXES) {
+  for (const unit in CONSTELLATION_UNITS) {
+    let unitMagnitudeID = CONSTELLATION_UNITS[unit].unitMagnitudeID;
+    let baseUnitOption = document.querySelector(`#${unitMagnitudeID} > option[value='0']`);
+
+    let prefixedUnitOption = document.createElement("option")
+    prefixedUnitOption.value = SI_PREFIXES[prefix];
+    prefixedUnitOption.textContent = prefix + unit;
+
+    if (SI_PREFIXES[prefix] > 0) {
+      baseUnitOption.after(prefixedUnitOption)
+    } else {
+      baseUnitOption.before(prefixedUnitOption)
+    }
+  }
+}
+
+function fillMetricUnitsWith(constellationToMetricFunc, constellationUnit) {
+  for (let [unit, value] of Object.entries(constellationToMetricFunc(constellationUnit))) {
     document.getElementById(unit).textContent = value.toLocaleString();
   }
 }
-fillMetricTimeWith(1);
 
-function updateTimeDisplay() {
-  fillMetricTimeWith(chronInput.value * Math.pow(12, chronMagnitude.value));
+function updateUnitDisplay(convertFunc, inputValueID, unitMagnitudeID) {
+  let unitMagnitude = document.getElementById(unitMagnitudeID).value;
+  let inputValue = document.getElementById(inputValueID).value;
+  fillMetricUnitsWith(convertFunc, inputValue * Math.pow(12, unitMagnitude));
 }
 
-let chronInput = document.getElementById("chrons");
-let chronMagnitude = document.getElementById("chronMagnitude");
-chronInput.addEventListener("input", updateTimeDisplay);
-chronMagnitude.addEventListener("input", updateTimeDisplay);
+for (const unit in CONSTELLATION_UNITS) {
+  let {inputValueID, unitMagnitudeID, convertFunc} = CONSTELLATION_UNITS[unit];
+  document.getElementById(inputValueID).addEventListener("input", () => updateUnitDisplay(convertFunc, inputValueID, unitMagnitudeID));
+  document.getElementById(unitMagnitudeID).addEventListener("input", () => updateUnitDisplay(convertFunc, inputValueID, unitMagnitudeID));
+
+  fillMetricUnitsWith(convertFunc, 1);
+}
